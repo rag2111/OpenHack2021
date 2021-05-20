605cea6e754e5964d6349778b084e480b94fc22b
Roberto Arocha <roberto.arocha@hexasystems.com>
2021-05-20 18:40:08 +0200

    Update Startup.cs

--- apis/poi/web/Startup.cs
+++ apis/poi/web/Startup.cs
@@ -24,7 +24,7 @@ namespace poi
         {
             Configuration = configuration;
         }
-
+HELLO
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
